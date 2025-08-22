d0e42e1e3caec992a88098c659c4e02d2337afb8
Priy Patel <priy.patel_ext@advaitbusiness.com>
2025-08-22 09:57:26 +0530

    Responsive Consulting

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,27 @@
+export default function ServiceCard({ sectionTitle, sectionTag, data }) {
+    return (
+        <section className="Consulting-section about-margin">
+            <div className="section-header">
+                {sectionTag && <p className="tag">{sectionTag}</p>}
+                <h2>
+                    <strong>{sectionTitle}</strong>
+                </h2>
+            </div>
+
+            <div className="workedwith-cards">
+                {data.map((item, index) => (
+                    <div className="workedwith-card" key={index}>
+                        <div className="workedwith-card-header">
+                            <h3>{item.title}</h3>
+                            <div className="arrow-box"></div>
+                        </div>
+                        <p>{item.description}</p>
+                        <div className="workedwith-src">
+                            <img src={item.src} alt={item.title} />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
--- src/page/Consulting.jsx
+++ src/page/Consulting.jsx
@@ -2,6 +2,7 @@ import industryVideo from "../assets/industry.mp4";
 import Consulting1 from '../assets/card1.jpg';
 import Consulting2 from '../assets/card2.jpg';
 import Consulting3 from '../assets/card3.jpg';
+import ServiceCard from '../components/ServiceCard';
 
 const Consultings = [
   {
@@ -39,44 +40,43 @@ const Consultings = [
 export default function Consulting() {
   return (
     <>
-    <section className="consulting-section">
-      <div className="video-banner">
-        <video
-          className="video-bg"
-          src={industryVideo}
-          autoPlay
-          loop
-          muted
-          playsInline
-        />
-        <div className="video-overlay">
-          <h1 className="consulting-title">
-            CONSULTING & ENGINEERING SERVICES
-          </h1>
+      <section className="industries-section about-margin">
+        <div className="video-banner">
+          <video
+            className="video-bg"
+            src={industryVideo}
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+          <div className="video-overlay">
+            <h1 className="industries-title">
+              CONSULTING & ENGINEERING SERVICES
+            </h1>
+          </div>
         </div>
-      </div>
 
-      <div className="consulting-content">
-        <div className="consulting-heading">
-          <h2>
-            <strong>Innovative IT Solutions</strong> Tailored
-            <br />
-            to Your Business Needs.
-          </h2>
+        <div className="industries-content service-content">
+          <div className="industries-heading">
+            <h2>
+              <strong>Innovative IT Solutions</strong>
+              <span className="mobile-br"><br /> </span>   Tailored           to Your Business Needs.
+            </h2>
+          </div>
+          <div className="industries-text">
+            <p>
+              Our SAP Managed Services ensure the seamless operation, maintenance,
+              and optimization of your SAP landscape. From system monitoring and
+              performance tuning to user support and patch management, we provide
+              end-to-end support tailored to your business needs. With proactive
+              management and 24/7 support, we help you reduce downtime, lower IT
+              costs, and enhance system performance.
+            </p>
+          </div>
         </div>
-        <div className="consulting-text">
-          <p>
-            Our SAP Managed Services ensure the seamless operation, maintenance,
-            and optimization of your SAP landscape. From system monitoring and
-            performance tuning to user support and patch management, we provide
-            end-to-end support tailored to your business needs. With proactive
-            management and 24/7 support, we help you reduce downtime, lower IT
-            costs, and enhance system performance.
-          </p>
-        </div>
-      </div>
-    </section>
-      <section className="Consulting-section">
+      </section>
+      {/* <section className="Consulting-section">
       <div className="section-header">
         <p className="tag">Our Consultings</p>
         <h2>
@@ -98,7 +98,8 @@ export default function Consulting() {
           </div>
         ))}
       </div>
-    </section>
+    </section> */}
+      <ServiceCard sectionTitle="Tailored Consultings" sectionTag="Our Consultings" data={Consultings} />
     </>
   );
 }
