60f0cf49b67025cfbc927df386b2fe4166d5e0f2
Jay-Advait <Jay@advaitbusiness.com>
2025-08-23 17:04:34 +0530

    changes

--- src/data/staticData.jsx
+++ src/data/staticData.jsx
@@ -252,3 +252,26 @@ export const footerData = {
   footerText: "© 2025 ADVAIT BUSINESS SOLUTION pvt ltd. All rights reserved.",
   privacyPolicy: "Privacy Policy",
 };
+
+//Company world map markers(WorldMap.jsx)
+export const markers = [
+  {
+    id: 1,
+    top: "59%",
+    left: "67%",
+    tooltip:
+      "Plot # 10: Sunshine Industrial Hub – 1, Near Zydus Lifesciences, Near Navapura Railway Crossing, Navapura, Changodar, Ahmedabad 382213. Gujarat. India",
+  },
+  {
+    id: 2,
+    top: "50%",
+    left: "25.5%",
+    tooltip: "3500 South Dupont Highway, Dover, County of Kent, DE 19901, USA",
+  },
+  {
+    id: 3,
+    top: "39%",
+    left: "46%",
+    tooltip: "46 Hibernia Road, Hounslow Middlesex TW3 3RY, United Kingdom",
+  },
+];
