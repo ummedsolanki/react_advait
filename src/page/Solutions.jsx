436bc7d462b2de5176563512b336f2ec1512e7e9
Priy Patel <priy.patel_ext@advaitbusiness.com>
2025-08-23 11:50:01 +0530

    Home-service responsive

--- src/page/Solutions.jsx
+++ src/page/Solutions.jsx
@@ -1,5 +1,5 @@
 import { solutionsData, solutionsText } from "../data/staticData";
-
+import ServiceCard from "../components/ServiceCard";
 const styles = {
   section: {
     fontFamily: "Open Sans, sans-serif",
@@ -9,7 +9,6 @@ const styles = {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    margin: "40px 0",
     flexWrap: "wrap",
     gap: "20px",
   },
@@ -23,7 +22,6 @@ const styles = {
     color: "#000",
     fontSize: "40px",
     fontWeight: 600,
-    marginBottom: "30px",
   },
   viewAll: {
     display: "inline-flex",
@@ -98,45 +96,36 @@ const styles = {
 
 export default function SolutionsSection() {
   return (
-    <section className="service-section">
-      <div style={styles.sectionHeader}>
-        <div>
-          <p style={styles.tag}>{solutionsText.solutionsTitle}</p>
-          <h2 style={styles.title}>
-            <strong>{solutionsText.solutionsSubTitle}</strong> {solutionsText.solutionsSubTitle2}
-            <br /> {solutionsText.solutionsSubTitle3}
-          </h2>
+    <>
+      <section className="service-section home-service">
+        <div className="home-section-header">
+          <div className="section-header home-section-title ">
+            <p className="tag">{solutionsText.solutionsTitle}</p>
+            <h2>
+              <strong>{solutionsText.solutionsSubTitle}</strong> {solutionsText.solutionsSubTitle2}
+              <span className="mobile-br"><br /> </span> {solutionsText.solutionsSubTitle3}
+            </h2>
+          </div>
+          <button className="apply-btn">
+            <span className="btn-text">{solutionsText.solutionsViewAll}</span>
+            <div className="job-box">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="16"
+                viewBox="0 0 32 16"
+                fill="currentColor"
+              >
+                <path d="M1.00024 7C0.447959 7 0.000244141 7.44772 0.000244141 8C0.000244141 8.55228 0.447959 9 1.00024 9V8V7ZM31.7074 8.70711C32.0979 8.31658 32.0979 7.68342 31.7074 7.29289L25.3434 0.928932C24.9529 0.538408 24.3197 0.538408 23.9292 0.928932C23.5387 1.31946 23.5387 1.95262 23.9292 2.34315L29.586 8L23.9292 13.6569C23.5387 14.0474 23.5387 14.6805 23.9292 15.0711C24.3197 15.4616 24.9529 15.4616 25.3434 15.0711L31.7074 8.70711ZM1.00024 8V9L31.0002 9V8V7L1.00024 7V8Z" />
+              </svg>
+            </div>
+          </button>
         </div>
-        <button className="apply-btn">
-          <span className="btn-text">{solutionsText.solutionsViewAll}</span>
-          <div className="job-box">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="16"
-              viewBox="0 0 32 16"
-              fill="currentColor"
-            >
-              <path d="M1.00024 7C0.447959 7 0.000244141 7.44772 0.000244141 8C0.000244141 8.55228 0.447959 9 1.00024 9V8V7ZM31.7074 8.70711C32.0979 8.31658 32.0979 7.68342 31.7074 7.29289L25.3434 0.928932C24.9529 0.538408 24.3197 0.538408 23.9292 0.928932C23.5387 1.31946 23.5387 1.95262 23.9292 2.34315L29.586 8L23.9292 13.6569C23.5387 14.0474 23.5387 14.6805 23.9292 15.0711C24.3197 15.4616 24.9529 15.4616 25.3434 15.0711L31.7074 8.70711ZM1.00024 8V9L31.0002 9V8V7L1.00024 7V8Z" />
-            </svg>
-          </div>
-        </button>
-      </div>
-
-      <div className="workedwith-cards">
-        {solutionsData.map((item, index) => (
-          <div className="workedwith-card" key={index}>
-            <div className="workedwith-card-header">
-              <h3>{item.title}</h3>
-              <div className="arrow-box"></div>
-            </div>
-            <p>{item.description}</p>
-            <div className="workedwith-src">
-              <img src={item.src} alt={item.title} />
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+      </section>
+      <ServiceCard
+        data={solutionsData}
+        className="home-section"
+      />
+    </>
   );
 }
