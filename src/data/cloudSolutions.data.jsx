05a481d99a99b10f1df5fa9b4bf51698e89299d4
Priy Patel <priy.patel_ext@advaitbusiness.com>
2025-08-25 17:00:16 +0530

    fix issue of data

--- src/data/cloudSolutions.data.jsx
+++ src/data/cloudSolutions.data.jsx
@@ -7,7 +7,6 @@ export const cloudStaticData = {
               drive digital transformation with our end-to-end SAP solutions.
               From strategic consulting to seamless implementation and ongoing
               support, we help you get the most value from your SAP investments.
-              <br />
               Whether you're starting your SAP journey or optimizing an existing
               environment, our expert team ensures scalable, future-ready
               solutions tailored to your business goals.`,
