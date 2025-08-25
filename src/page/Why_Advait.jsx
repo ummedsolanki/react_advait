3c38d0ce6a24900957621b22ba2518fe61af16a2
Priy Patel <priy.patel_ext@advaitbusiness.com>
2025-08-25 12:04:52 +0530

    Home Why Responsive

--- src/page/Why_Advait.jsx
+++ src/page/Why_Advait.jsx
@@ -35,20 +35,15 @@ export default function Why() {
         <div
           className="banner-container"
           style={{
-            height: "50px",
             background: "white",
-            clipPath: "polygon(0px 0px, 50% 0px, 75% 360%, 100% 100%, 0% 100%)",
-            paddingTop: "10px",
+            clipPath: "polygon(0px 0px, 50% 0px, 70% 360%, 100% 100%, 0% 100%)",
+            // paddingTop: "10px",
             overflow: "hidden",
-            top: "-30px",
           }}
         >
           <div className="banner-left">
             <p className="banner-subtitle">{whyText.whyAdvait}</p>
-            <h1 className="banner-title">
-              {whyText.whyAdvaitSub} <br />
-              <span className="banner-title-thin">{whyText.whyAdvaitSub2}</span>
-            </h1>
+
           </div>
         </div>
       </section>
@@ -56,6 +51,10 @@ export default function Why() {
       {/* Feature Section */}
       <section className="section5">
         <div className="section5_left">
+          <h1 className="banner-title">
+            {whyText.whyAdvaitSub} <br />
+            <span className="banner-title-thin">{whyText.whyAdvaitSub2}</span>
+          </h1>
           <div className="sec5_cards">
             <div className="section5_grid">
               {whyCardData.map((card, index) => (
