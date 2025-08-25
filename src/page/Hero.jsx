ed29068676f517a3009648610b1a539473f280b7
Priy Patel <priy.patel_ext@advaitbusiness.com>
2025-08-25 15:02:32 +0530

    fix

--- src/page/Hero.jsx
+++ src/page/Hero.jsx
@@ -50,7 +50,7 @@ export default function Hero() {
         autoPlay
         muted
         playsInline
-       
+
       >
         <source src={heroVideo} type="video/mp4" />
         Your browser does not support the video tag.
