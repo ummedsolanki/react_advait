48650d75046812be1bcf34d3270a6f6482c54bff
Priy Patel <priy.patel_ext@advaitbusiness.com>
2025-08-30 15:10:30 +0530

    fix index

--- src/data/aboutUs.data.jsx
+++ src/data/aboutUs.data.jsx
@@ -2,17 +2,17 @@
 
 import aboutImage from "../assets/About-us.jpg";
 export const aboutUsEmp = {
-    title: "About Us",
-    aboutImage: aboutImage,
+  title: "About Us",
+  aboutImage: aboutImage,
 }
 
 export const aboutUsEmpContent = {
-    title1:"Empowering Your Digital ",
-    title2:"Journey",
-    title3:"with Clarity and",
-    title4:"Confidence",
-    para1:"Advait Business Solutions is a trusted global technology partner with over 13 years of experience in delivering innovative, scalable enterprise solutions. Headquartered in Ahmedabad, India, the company supports organizations worldwide in their digital transformation journeys. Advait specializes in implementing and optimizing advanced technologies including SAP S/4HANA, RISE with SAP, Microsoft Dynamics, Odoo ERP, Analytics, AI/ML, and Custom Application Development.",
-    para2:"With a deep commitment to quality and a customer-first mindset, Advait empowers businesses across industries to enhance operational efficiency, embrace digital innovation, and achieve measurable business outcomes.",
+  title1: "Empowering Your Digital ",
+  title2: "Journey",
+  title3: "with Clarity and",
+  title4: "Confidence",
+  para1: "Advait Business Solutions is a trusted global technology partner with over 13 years of experience in delivering innovative, scalable enterprise solutions. Headquartered in Ahmedabad, India, the company supports organizations worldwide in their digital transformation journeys. Advait specializes in implementing and optimizing advanced technologies including SAP S/4HANA, RISE with SAP, Microsoft Dynamics, Odoo ERP, Analytics, AI/ML, and Custom Application Development.",
+  para2: "With a deep commitment to quality and a customer-first mindset, Advait empowers businesses across industries to enhance operational efficiency, embrace digital innovation, and achieve measurable business outcomes.",
 }
 
 //About Mission
@@ -21,7 +21,7 @@ import card2 from '../assets/our2.jpg';
 import card3 from '../assets/our3.jpg';
 
 export const missionStaticData = {
-    title: 'Our Mission',
+  title: 'Our Mission',
 }
 
 export const aboutMissions = [
@@ -49,29 +49,29 @@ export const aboutMissions = [
 //Timeline Data
 export const timelineData = [
   {
-    year: "2012",
+    year: "2012-2015",
     text: "The Begining ADVAIT journey began in 2012",
   },
   {
-    year: "2013",
+    year: "2016-2019",
     text: "Partnered with Accenture India The first partnership happened with and we were responsible for delivering part of the large projects undertaken.",
   },
   {
-    year: "2014 - 2015",
+    year: "2020 - 2023",
     text: "Revenue Grew by 160% & Team Grew by 25% since inception and there was a considerable amount of revenue growth & team during 2014-2015.",
   },
   {
-    year: "2016",
+    year: "2024 - Present",
     text: "Delivery Centre Setup in Ahmedabad, Gujarat. India Inaugurated a new office in Ahmedabad for setting up a delivery center for Gujarat.",
   },
-  {
-    year: "2017",
-    text: "Attained CMMi Level 3 certification for non-SAP practices, Recognition by DIPP for an upcoming startup from the government.",
-  },
-  {
-    year: "2018",
-    text: "Team Grew Team Grew by 15% and Revenues by 30%. Performed Upgrades, Migrations and implementations on SAP.",
-  },
+  // {
+  //   year: "2017",
+  //   text: "Attained CMMi Level 3 certification for non-SAP practices, Recognition by DIPP for an upcoming startup from the government.",
+  // },
+  // {
+  //   year: "2018",
+  //   text: "Team Grew Team Grew by 15% and Revenues by 30%. Performed Upgrades, Migrations and implementations on SAP.",
+  // },
 ];
 
 //Team Members Data
@@ -86,11 +86,11 @@ import img4Hover from "../assets/team1.jpg";
 import linkedinIcon from "../assets/linkedin.svg";
 
 export const teamStaticData = {
-    title: "Our Team",
-    subtitle: "A team of experts ",
-    subtitle2: "dedicated",
-    subtitle3: "to your success.",
-    linkedinImage: linkedinIcon,
+  title: "Our Team",
+  subtitle: "A team of experts ",
+  subtitle2: "dedicated",
+  subtitle3: "to your success.",
+  linkedinImage: linkedinIcon,
 }
 
 export const teamMembers = [
